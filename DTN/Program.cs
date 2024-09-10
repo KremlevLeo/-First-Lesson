515e71f806d74680bb3c83fe948052fa154b85dc
KremlevLeonid <leeonidix@gmail.com>
2024-09-10 16:16:13 +0500

    Add Hard Hello User

---
+++ DTN/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace DTN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            double b = double.Parse(Console.ReadLine());
+            double res = a / b;
+            Console.WriteLine($"The division is: {res}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Hard Hello User/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Hard_Hello_User
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string userName = Console.ReadLine();
+            if (userName == "Masha")
+            {
+                Console.WriteLine($"Oh! Hi {userName}");
+            }
+            else { 
+            Console.WriteLine($"Hello, {userName}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
