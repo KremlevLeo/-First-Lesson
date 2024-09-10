d5c2f004537fa29a917a94a6a78a319b58a5dea1
KremlevLeonid <leeonidix@gmail.com>
2024-09-10 14:56:17 +0500

    Add Hello user

---
+++ Hello user/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Hello_user
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write($"Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}");
+            Console.ReadLine();
+        }
+    }
+}
