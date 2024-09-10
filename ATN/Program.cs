cf50e231f6649fb331ceb5ce60422486a0edf0bd
KremlevLeonid <leeonidix@gmail.com>
2024-09-10 15:19:12 +0500

    Add Adding Two Numbers

---
+++ ATN/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ATN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            int b = int.Parse(Console.ReadLine());
+            int res = a+b;
+            Console.WriteLine($"The amount is equal to: {res}");
+            Console.ReadLine();
+        }
+    }
+}
